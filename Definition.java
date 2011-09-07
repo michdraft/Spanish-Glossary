fd81be21e4771ca1896a6576cae3e022633cc06f
Jonathan Broome <jcbroome@gmail.com>
2011-09-06 21:11:43 -0500

    created Definition class and a tester main

---
+++ Definition.java
@@ -0,0 +1,21 @@
+
+
+class Definition {
+
+	String span;
+	String eng;
+         
+	public Definition(String sp, String en) {
+    	span = sp;
+		eng = en;
+	}
+
+	public String getSpanish() {
+		return span;
+	}
+
+	public String getEnglish() {
+     	return eng;
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+ 	public static void main(String[] args) {
+     	Definition d = new Definition("Hola!", "Hey!");
+		System.out.println(d.getEnglish());
+	}
+}
